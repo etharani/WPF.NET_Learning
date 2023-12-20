4a892fc90395e4ff2eddf66131803e4ab3d0e561
etharani <tharani2297@gmail.com>
2023-12-20 18:37:42 +0530

    Code behind , Grid control

---
+++ WpfApplication2/WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool running = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void checkbtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (running)
+            {
+                //stop 
+                txtblock.Text = "Stoped";
+                checkbtn.Content = "Run";
+            }
+            else
+            {
+                //run
+                txtblock.Text = "Running";
+                checkbtn.Content = "Stop";
+
+            }
+            running = !running;
+        }
+    }
+}
