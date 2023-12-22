a4a9c38775a772333cae3002084e2ac249041ebd
etharani <tharani2297@gmail.com>
2023-12-22 20:28:40 +0530

    Folder browser dialog

---
+++ WpfApplication2/WpfApplication2/Windows.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+//using System.Windows.Forms;
+using winforms=System.Windows.Forms; 
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for Windows.xaml
+    /// </summary>
+    public partial class Windows : Window
+    {
+        public Windows()
+        {
+            InitializeComponent();
+        }
+
+        private void browerbtn_Click(object sender, RoutedEventArgs e)
+        {
+            // FolderBrowserDialog dialog = new FolderBrowserDialog();
+            winforms.FolderBrowserDialog dialog = new winforms.FolderBrowserDialog();
+            dialog.InitialDirectory = "D:.NET_WPF";
+            dialog.ShowDialog();
+        //  Note    MessageBox.Show("");
+        }
+    }
+}
