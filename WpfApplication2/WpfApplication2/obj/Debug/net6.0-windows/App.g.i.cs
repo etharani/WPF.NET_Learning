2231e8c7a35e5e3be2bc5e3290144bff3cde40a4
etharani <tharani2297@gmail.com>
2024-01-01 23:53:43 +0530

    Adding to modify

--- WpfApplication2/WpfApplication2/obj/Debug/net6.0-windows/App.g.i.cs
+++ WpfApplication2/WpfApplication2/obj/Debug/net6.0-windows/App.g.i.cs
@@ -46,7 +46,7 @@ namespace WpfApplication2 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -61,7 +61,7 @@ namespace WpfApplication2 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         public static void Main() {
             WpfApplication2.App app = new WpfApplication2.App();
             app.InitializeComponent();
